ecb3b6c2fdf3fd4a2250dc3ca375ae3b29dd9f5a
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-12-26 11:34:08 +0530

    Add files via upload

---
+++ sortingarray.java
@@ -0,0 +1,20 @@
+class Main {
+    public static void main(String[] args) {
+        int arr[]={9,3,2,6,7};
+        int temp=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.println();
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
